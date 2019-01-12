aeee582274c7ccf1f7ba7889daf1a9c6c0604429
whitepiolin <whitepiolin@gmail.com>
2019-01-13 00:22:58 +0100

    moneyConverter

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Index from "./router/Index";
+import MoneyConverter from "./components/MoneyConverter";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Index />
+        <MoneyConverter />
       </div>
     );
   }
