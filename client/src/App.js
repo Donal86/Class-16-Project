9270d52e90e71c024b5567a0a180e354b4d23a57
mehmetscoskun <mehmetsaidcoskun@gmail.com>
2019-01-13 00:29:32 +0100

    map 2.2

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Index from "./router/Index";
+import HouseMap from "./components/HouseMap.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Index />
+        <HouseMap/>
       </div>
     );
   }
--- client/src/components/HouseMap.js
+++ client/src/components/HouseMap.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react"
 import './map.css';
 import { YMaps, Map, Clusterer, Placemark } from "react-yandex-maps";
+import { inject, observer } from 'mobx-react';
+
+@inject('PropertiesStore')
+@observer
 
 
 class HouseMap extends React.Component {
+
+    constructor(props) {
+        super(props);
+        props.PropertiesStore.listProperties();
+    }
+
     render () {
         //it takes all houses' properties as a json file.
-        const {houses} = this.props;
+        const { PropertiesStore } = this.props;
+        // const {houses} = this.props;
         
         //when clicked a placemarker, you are going to see this content.
         //in the next step, I am going to add <a> for the detailed properties of each house
@@ -15,7 +26,7 @@ class HouseMap extends React.Component {
         }
 
         //all placemarkers are here. I created it with map function
-        const markerHouses = houses.map((house, key) => 
+        const markerHouses = PropertiesStore.map((house, key) => 
             <Placemark
                 key={key} 
                 geometry={[house.lat, house.lng]}
@@ -31,13 +42,13 @@ class HouseMap extends React.Component {
         
         // where should be the starting point? I think averages of the lats and lngs can be a good place.
         //because it is the exact middle of the all placemarkers.
-        let startingCenter = houses.reduce((accu, house) => {
+        let startingCenter = PropertiesStore.reduce((accu, house) => {
             if(accu['lat'] === undefined || accu['lng'] === undefined) {
                 accu['lat'] = 0;
                 accu['lng'] = 0;
             } else {
-                accu['lat'] = accu['lat'] + house.lat / houses.length;
-                accu['lng'] = accu['lng'] + house.lng / houses.length;
+                accu['lat'] = accu['lat'] + house.lat / PropertiesStore.length;
+                accu['lng'] = accu['lng'] + house.lng / PropertiesStore.length;
             }
             return accu
         }, {})
