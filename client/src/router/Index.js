0e2a73fefd17825af4adb7424d6f524563005e78
mehmetscoskun <mehmetsaidcoskun@gmail.com>
2019-01-13 01:52:22 +0100

    apidoc 1.0

--- client/src/router/Index.js
+++ client/src/router/Index.js
@@ -7,6 +7,7 @@ import NoMatch from "../components/NoMatch";
 import CityChart from '../components/stats/CityChart';
 import HouseDetails from '../components/houseDetails'
 import CurrencyConverter from "../components/currencyConverter"
+import Apidoc from "../components/Apidoc";
 
 class Index extends React.Component {
   render() {
@@ -55,6 +56,16 @@ class Index extends React.Component {
                 Display charts
 							</NavLink>
             </li>
+            <li>
+              <NavLink
+                className="nav-link"
+                to={{ pathname: "doc" }}
+                exact
+                activeStyle={{ color: "gray" }}
+              >
+                Api Documentation
+              </NavLink>
+            </li>
           </ul>
           <Switch>
             <Route
@@ -68,6 +79,7 @@ class Index extends React.Component {
             <Route path="/contribute" exact component={Contribute} />
             <Route path="/citychart/:city?" component={CityChart} />
             <Route path='/house/:id?' component={HouseDetails} />
+            <Route path="/doc" component={Apidoc} />
             <Route component={NoMatch} />
           </Switch>
         </div>
