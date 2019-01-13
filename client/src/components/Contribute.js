058e6a2944d9bbdd158cf7c9b0823705d8ef354e
sima <sima.milli@gmail.com>
2019-01-13 15:38:39 +0100

    adding contribute page

--- client/src/components/Contribute.js
+++ client/src/components/Contribute.js
@@ -1,33 +1,37 @@
 import React from "react";
 import { inject, observer } from "mobx-react";
-// import ResultsTable from "../ResultsTable";
+import ResultsTable from "../components/ResultsTable";
 
 @inject("PropertiesStore")
 @observer
 class Contribute extends React.Component {
   insertJson = e => {
     e.preventDefault();
-    const jsonFromText = { type: "json", json: this.inputFromText.value };
-    const jsonFromUrl = { type: "url", url: this.inputFromUrl.value };
-    this.props.PropertiesStore.createProperty(jsonFromText, jsonFromUrl);
+    if (this.inputFromText.value) {
+      const jsonFromText = { type: "json", json: this.inputFromText.value };
+      this.props.PropertiesStore.createProperty(jsonFromText);
+    } else if (this.inputFromUrl.value) {
+      const jsonFromUrl = { type: "url", url: this.inputFromUrl.value };
+      this.props.PropertiesStore.createProperty(jsonFromUrl);
+    }
   };
   render() {
-    // const { PropertiesStore } = this.props;
-    // const insertResults = PropertiesStore.properties.details.length ? (
-    //   <ResultsTable />
-    // ) : null;
+    const { PropertiesStore } = this.props;
+    const insertResults = PropertiesStore.properties.details.length ? (
+      <ResultsTable />
+    ) : null;
     return (
       <div className="pages">
         <h2>Contribute with us! add your properties</h2>
         <p>
-          See our our contribution conditions
+          See our contribution conditions
           <a href="http://www.google.com"> here</a>.
         </p>
         <div className="insert-form">
           {this.props.PropertiesStore.properties.insertStatus === "error" ? (
             <p className="error">
-              Please make sure you entered valid inputs{" "}
-              <a href="http://www.google.com">see contribution conditions</a>.
+              Please make sure you entered valid inputs
+              <a href="http://www.google.com"> see contribution conditions</a>.
             </p>
           ) : this.props.PropertiesStore.properties.insertStatus === "done" ? (
             <p className="succeed">Valid!</p>
@@ -40,14 +44,15 @@ class Contribute extends React.Component {
                   placeholder="insert JSON"
                   ref={input => (this.inputFromText = input)}
                 />
-              </div>
-              <div className="col">
-                <input
-                  type="url"
-                  className="form-control"
-                  placeholder="Enter api for JSON url"
-                  ref={input => (this.inputFromUrl = input)}
-                />
+                <div />
+                <div>
+                  <input
+                    type="url"
+                    className="form-control"
+                    placeholder="Enter api for JSON url"
+                    ref={input => (this.inputFromUrl = input)}
+                  />
+                </div>
               </div>
             </div>
             <div className="col-auto">
@@ -57,7 +62,7 @@ class Contribute extends React.Component {
             </div>
           </form>
         </div>
-        {/* <div>{insertResults}</div> */}
+        <div>{insertResults}</div>
       </div>
     );
   }
