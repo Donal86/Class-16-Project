37a21286889b56c3efcb070292bb739b9621cd71
AllouhM <mahmoud.allouh0@gmail.com>
2019-01-15 14:09:48 +0100

    changes because of rebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import Index from "./router/Index";
 import MoneyConverter from "./components/MoneyConverter";
-=======
-import React, { Component } from 'react';
-import Index from './router/Index';
->>>>>>> remove chart.css from index
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Index />
-<<<<<<< HEAD
         <MoneyConverter />
-=======
->>>>>>> fix get cities flow
       </div>
     );
   }
