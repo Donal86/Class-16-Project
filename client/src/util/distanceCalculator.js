e36e9498458d13542972f910e4fd2f08d1f90964
mehmetscoskun <mehmetsaidcoskun@gmail.com>
2019-01-16 20:46:26 +0100

    map 2.7

--- client/src/util/distanceCalculator.js
+++ client/src/util/distanceCalculator.js
@@ -1,21 +1,19 @@
-function degreesToRadians (degrees) {
-  return (degrees * Math.PI) / 180
+function toRad(Value) {
+  return Value * Math.PI / 180;
 }
 
-function distanceCalculator (lat1, lon1, lat2, lon2) {
-  var earthRadiusKm = 6371
+function distanceCalculator(lat1, lon1, lat2, lon2) {
+  var R = 6371; // km
+  var dLat = toRad(lat2 - lat1);
+  var dLon = toRad(lon2 - lon1);
+  var lat1Rad = toRad(lat1);
+  var lat2Rad = toRad(lat2);
 
-  var dLat = degreesToRadians(lat2 - lat1)
-  var dLon = degreesToRadians(lon2 - lon1)
-
-  lat1 = degreesToRadians(lat1)
-  lat2 = degreesToRadians(lat2)
-
-  var a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2)
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-  return earthRadiusKm * c
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1Rad) * Math.cos(lat2Rad);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
 }
 
 export default distanceCalculator
