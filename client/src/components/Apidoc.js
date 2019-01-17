0804fd685a3f60acdf57d53e7319e7d085c669b0
mehmetscoskun <mehmetsaidcoskun@gmail.com>
2019-01-17 23:56:26 +0100

    apidoc 2.1

--- client/src/components/Apidoc.js
+++ client/src/components/Apidoc.js
@@ -43,19 +43,19 @@ class Apidoc extends React.Component {
                     </a>
         
                     <a href="#getting">
-                        <h3>Getting JSON File</h3>
+                        <h3>Final Products of API</h3>
                     </a>
         
                     <a href="#contribution">
-                        <h3>Contribution</h3>
-                    </a>
-
-                    <a href="#about">
-                    <h3>About Us</h3>
+                        <h3>How to Contribute</h3>
                     </a>
 
                     <a href="#source">
                     <h3>Sources of The Database</h3>
+                    </a>
+
+                    <a href="#about">
+                    <h3>About Us</h3>
                     </a>
                 </div>
         
@@ -72,21 +72,51 @@ class Apidoc extends React.Component {
                     </div>
         
                     <div id='getting'>
-                        <h2>Getting JSON File</h2>
-                        <p>getting json file bla bla bla</p>
+                        <h2>Final Products of API</h2>
+                        <h3>1- Showing Data</h3>
+                        <p>This API is able to show information/data from worldwide real estate market. Users reach prices, 
+                        spesifications and locations of houses for sale in different countries or cities. Users can reach the 
+                        data according to filter options or they can sort selected data tables according to preferences. </p>
+
+                        <h3>2- Producing a JSON File</h3>
+                        <p>This API is able to produce a JSON file that consists comprehensive data of houses for sale in real 
+                        estate market. </p>
+                        <h3>3- Presenting Statistics</h3>
+                        <p>This API is able to show charts of statistics about daily average prices of market in terms of average 
+                        price per m2 or average price per house. Users can analyze the avarage price rates with according to 
+                        filter options. </p>
                     </div>
         
                     <div id='contribution'>
                             <h2>How to Contribute</h2>
-                            <h3>As a URL</h3>
-                            <p>contrasdasdasdasdasd json file bla bla bla</p>
+                            <p>Of course, everyone can contribute to our database. Therefore, the other people can
+                            also reach an extensive real estate data. When you make a contribution, your contributed
+                            data will be processed with our validation function. All of your valid elements will be insterted
+                            into our database and then you will get a report for your invalid elements.Our database is open to 
+                            contribution in three different ways:</p>
 
-                            <h3>As a JSON File</h3>
-                            <p>asdasddasdasd</p>
+                            <h3>a. As a URL</h3>
+                            <p>You can fetch and contribute by using the URL of the resource. Typically this is an 
+                            absolute URL without the host component, e.g. "/path". If the URL has the host of another 
+                            site, the request is performed.
+                            <br/>
+                            Any non-Request object will be converted to a String, making it possible to pass an instance of URL, 
+                            for example.
+                            <br/>
+                            A request can be initialized using another instance of Request in place of the URL. In that case, 
+                            the URL and other options are inherited from the provided Request object.</p>
 
-                            <h3>As a JSON Text</h3>
+                            <h3>b. As a JSON File</h3>
+                            <p>You can upload your JSON file into our database as well. Only thing you should do is to select 
+                            your file and click the upload button.</p>
+
+                            <h3>c. As a JSON Text</h3>
+                            <p>In this opportunity of contribution, you should copy your JSON file and paste to our text area.
+                            Then just click the upload button and leave the rest to us.</p>
+
+
                             <div className="entryFormat">
-                                <h3>Please make sure that</h3>
+                                <h3>For all types of contribution please make sure that</h3>
                                 <ul>
                                     <li>
                                         Your JSON file must be an array.
@@ -111,7 +141,13 @@ class Apidoc extends React.Component {
 
                     <div id='about'>
                     <h2>About Us</h2>
-                    <p>gettasd;'lkas'daslkdfile bla bla bla</p>
+                    <p>This API is produced as an educational project by the students of Hack Your Future coding school</p>
+                    <p> HackYourFuture is a coding school that provides newcomers with a refugee status an opportunity to 
+                    build and improve programming skills. Over the course of seven months professional developers teach 
+                    HTML, CSS, GitHub, JavaScript, Node, Databases and React. Problem-solving skills and teamwork are 
+                    additional parts of the curriculum. In the last part of the course, students build a final project 
+                    to put into practice what they have learned. This API is also a final project and involves mainly the 
+                    broad techniques of front-end web developing. </p>
                     </div>
                 </div>
             </div>
