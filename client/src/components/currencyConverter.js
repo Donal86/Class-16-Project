dc72df81983dae2acd2fe3421be5fb83c515cd8f
whitepiolin <whitepiolin@gmail.com>
2019-01-18 18:55:23 +0100

    localStorageInsert

--- client/src/components/currencyConverter.js
+++ client/src/components/currencyConverter.js
@@ -4,19 +4,22 @@ import { inject, observer } from "mobx-react";
 @inject("PropertiesStore")
 @observer
 class CurrencyConverter extends React.Component {
+  constructor(props) {
+    super(props);
+    props.PropertiesStore.listCurrencies();
+  }
+
   render() {
     const { PropertiesStore } = this.props;
-    const currencies = [
-      "EUR", "USD", "GBP", "RUB", "TRY", "HRK", "HUF", "IDR", "PHP",  "RON", "ISK", "SEK", "THB", "PLN", "CAD", "AUD", "MYR", "NZD", "CHF", "DKK", "SGD", "CNY", "BGN", "CZK", "BRL", "JPY", "KRW", "INR", "MXN", "HKD", "ZAR", "ILS", "NOK"];
+
     return (
       <div>
         <div className="Converter">
           <select
-            name="to"
             onChange={event => PropertiesStore.selectHandler(event)}
             value={PropertiesStore.properties.toCurrency}
           >
-            {currencies.map(currency => (
+            {PropertiesStore.properties.currencies.map(currency => (
               <option key={currency}>{currency}</option>
             ))}
           </select>
