fddf88a49b91c87976f84bc355cc0b53484e5108
AllouhM <mahmoud.allouh0@gmail.com>
2019-01-19 03:23:22 +0100

    after accept current change-rebase

--- client/src/router/Index.js
+++ client/src/router/Index.js
@@ -1,44 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 import List from '../components/List';
-
-class Index extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to='/' exact="true">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to='/list' exact="true">
-                List
-              </Link>
-            </li>
-          </ul>
-          <Route
-            path='/'
-            exact={true}
-            render={() => {
-              return <h2>Welcome Home</h2>;
-            }}
-          />
-
-          <Route path='/list' component={List} />
-        </div>
-      </Router>
-    );
-  }
-=======
-import { BrowserRouter as Router, Link, Switch } from 'react-router-dom';
-import Route from 'react-router-dom/Route';
-import List from '../components/List';
-import CityChart from '../components/stats/CityChart';
 
 class Index extends React.Component {
 	render() {
@@ -47,37 +10,29 @@ class Index extends React.Component {
 				<div>
 					<ul>
 						<li>
-							<Link to="/" exact={true}>
+							<Link to='/' exact="true">
 								Home
-							</Link>
+              </Link>
 						</li>
 						<li>
-							<Link to="/list" exact={true}>
+							<Link to='/list' exact="true">
 								List
-							</Link>
-						</li>
-						<li>
-							<Link to={{ pathname: "citychart" }} exact={true}>
-								Display charts
-							</Link>
+              </Link>
 						</li>
 					</ul>
-					<Switch>
-						<Route
-							path="/"
-							exact={true}
-							render={() => {
-								return <h2>Welcome Home</h2>;
-							}}
-						/>
-						<Route path="/list" component={List} />
-						<Route path="/citychart/:city?" component={CityChart} />
-					</Switch>
+					<Route
+						path='/'
+						exact={true}
+						render={() => {
+							return <h2>Welcome Home</h2>;
+						}}
+					/>
+
+					<Route path='/list' component={List} />
 				</div>
 			</Router>
 		);
 	}
->>>>>>> add chart code
 }
 
 export default Index;
