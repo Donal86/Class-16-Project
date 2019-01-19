f5792590880f01d0a67e929de3eecedf6d01b24c
Denys Izhetskyi <mail.bigjey@gmail.com>
2019-01-19 21:54:42 +0100

    list view fixes

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -63,6 +63,10 @@ class List extends React.Component {
   handleChange = (field, value) => {
     this.params[field] = value;
 
+    if (field !== 'page') {
+      this.params.page = 1;
+    }
+
     this.props.history.push({
       ...this.props.location,
       search: this.createQueryString()
@@ -74,143 +78,184 @@ class List extends React.Component {
   render() {
     const pages = Math.ceil(this.state.total / 5);
     return (
-      <div className='List-page'>
-        <div className='List-sidebar'>
-          {this.state.total === 1 ? <p> Found {this.state.total} house </p> : <p> Found {this.state.total} houses </p>}
-          <select
-            value={this.params.city}
-            onChange={e => {
-              this.handleChange('city', e.target.value);
-            }}>
-            <option value=''>select city</option>
-            {this.state.countryCity
-              .sort((a, b) => {
-                if (a.location_city > b.location_city) {
-                  return 1;
-                } else {
-                  return -1;
-                }
-              })
-              .map((field, i) => (
-                <option key={i} value={field.location_city}>
-                  {field.location_city}
-                </option>
-              ))}
-          </select>
-          <br />
-          <select
-            value={this.params.country}
-            onChange={e => {
-              this.handleChange('country', e.target.value);
-            }}>
-            <option value=''>select country</option>
-            {this.state.countryCity
-              .sort((a, b) => {
-                if (a.location_country > b.location_country) {
-                  return 1;
-                } else {
-                  return -1;
-                }
-              })
-              .map((field, i) => (
-                <option key={i} value={field.location_country}>
-                  {field.location_country}
-                </option>
-              ))}
-          </select>
-          <input
-            type='number'
-            value={this.params.price_min}
-            onChange={e => {
-              this.handleChange('price_min', e.target.value);
-            }}
-          />
-          <input
-            type='number'
-            value={this.params.price_max}
-            onChange={e => {
-              this.handleChange('price_max', e.target.value);
-            }}
-          />
+      <React.Fragment>
+        <div className='List-page'>
+          <div className='List-sidebar'>
+            <div className="List-filters">
 
-          <div
-            onChange={e => {
-              this.handleChange('rooms', e.target.value);
-            }}>
-            <input type='radio' name='rooms' value='1' />
-            1+
-            <input type='radio' name='rooms' value='2' />
-            2+
-            <input type='radio' name='rooms' value='3' />
-            3+
-            <input type='radio' name='rooms' value='4' />
-            4+
-            <input type='radio' name='rooms' value='5' />
-            5+
-          </div>
-        </div>
+              <div className="List-filter">
+                <div className="List-filter-label">Country</div>
+                <select
+                  value={this.params.country}
+                  onChange={e => {
+                    this.handleChange('country', e.target.value);
+                  }}>
+                  <option value=''>select country</option>
+                  {this.state.countryCity
+                    .sort((a, b) => {
+                      if (a.location_country > b.location_country) {
+                        return 1;
+                      } else {
+                        return -1;
+                      }
+                    })
+                    .map((field, i) => (
+                      <option key={i} value={field.location_country}>
+                        {field.location_country}
+                      </option>
+                    ))}
+                </select>
+              </div>
 
-        <select
-          className='List-sorting'
-          value={this.params.order}
-          onChange={e => {
-            this.handleChange('order', e.target.value);
-          }}>
-          {['price_value', 'market_date'].map((field, i) => (
-            <React.Fragment key={i}>
-              <option value={`${field}_asc`}>{field} asc</option>
-              <option value={`${field}_desc`}>{field} desc</option>
-            </React.Fragment>
-          ))}
-        </select>
 
-        {this.state.data.length === 0 ? (
-          <div className='List-search_no_results'>
-            <h2>0 results </h2>{' '}
-          </div>
-        ) : (
-            <div className='List-main'>
-              {this.state.data.map(property => (
-                <div className='List' key={property.id}>
-                  {property.location_city}
-                  <br />
-                  {property.location_country}
-                  <br />
-                  Rooms {property.size_rooms}
-                  <br />
-                  {property.location_address}
-                  <br />
-                  Price {property.price_value}
-                  <br />
-                  <a href='http://localhost:3000/list'>{property.link}</a>
-                  <br />
-                  {property.images && <img width='200' alt='' src={property.images.split(',')[0]} />}
-                  <br />
-                  {property.market_date}
+              <div className="List-filter">
+                <div className="List-filter-label">City</div>
+                <select
+                  value={this.params.city}
+                  onChange={e => {
+                    this.handleChange('city', e.target.value);
+                  }}>
+                  <option value=''>select city</option>
+                  {this.state.countryCity
+                    .sort((a, b) => {
+                      if (a.location_city > b.location_city) {
+                        return 1;
+                      } else {
+                        return -1;
+                      }
+                    })
+                    .map((field, i) => (
+                      <option key={i} value={field.location_city}>
+                        {field.location_city}
+                      </option>
+                    ))}
+                </select>
+              </div>
+
+              <div className="List-filter">
+                <div className="List-filter-label">Price</div>
+                <input
+                  type='number'
+                  value={this.params.price_min}
+                  onChange={e => {
+                    this.handleChange('price_min', e.target.value);
+                  }}
+                />
+                <input
+                  type='number'
+                  value={this.params.price_max}
+                  onChange={e => {
+                    this.handleChange('price_max', e.target.value);
+                  }}
+                />
+              </div>
+
+              <div className="List-filter">
+                <div className="List-filter-label">Number of rooms</div>
+                <div
+                  onChange={e => {
+                    this.handleChange('rooms', e.target.value);
+                  }}>
+                  <label>
+                    <input type='radio' name='rooms' value='1' />
+                    1+
+                  </label>
+                  <label>
+                    <input type='radio' name='rooms' value='2' />
+                    2+
+                  </label>
+                  <label>
+                    <input type='radio' name='rooms' value='3' />
+                    3+
+                  </label>
+                  <label>
+                    <input type='radio' name='rooms' value='4' />
+                    4+
+                  </label>
+                  <label>
+                    <input type='radio' name='rooms' value='5' />
+                    5+
+                  </label>
                 </div>
-              ))}
+              </div>
             </div>
-          )}
+          </div>
 
-        <div className='List-pagination'>
-          {pages > 0 && (
-            <ul>
-              {Array(pages)
-                .fill(0)
-                .map((el, index) => (
-                  <li
-                    key={index}
-                    onClick={() => {
-                      this.handleChange('page', index + 1);
-                    }}
-                    className={`${this.params.page - 1 === index ? 'active' : ''}`}>
-                    {index + 1}
-                  </li>
+          <div className="List-main">
+            {this.state.total === 1 ? <div className="List-total"> Found {this.state.total} house </div> : <div className="List-total"> Found {this.state.total} houses </div>}
+
+            <div className="List-filter">
+              Order by: &nbsp;
+              <select
+                value={this.params.order}
+                onChange={e => {
+                  this.handleChange('order', e.target.value);
+                }}>
+                {['price_value', 'market_date'].map((field, i) => (
+                  <React.Fragment key={i}>
+                    <option value={`${field}_asc`}>{field} asc</option>
+                    <option value={`${field}_desc`}>{field} desc</option>
+                  </React.Fragment>
                 ))}
-            </ul>
-          )}
+              </select>
+            </div>
+
+            {this.state.data.length === 0 ? (
+              <div className='List-search_no_results'>
+                <h2>0 results </h2>{' '}
+              </div>
+            ) : (
+                <div className='List-main'>
+                  {this.state.data.map(property => (
+                    <a className='List' key={property.id}>
+                      <div className="List-img" style={{
+                        backgroundImage: property.images ? `url(${property.images.split(',')[0]})` : ''
+                      }}>
+                      </div>
+                      <div className="List-info">
+                        <div className="List-info--price">Price {property.price_value}</div>
+                        <div className="List-info--address">
+                          {property.location_address} <br />
+                          {property.location_city}, {property.location_country}
+                        </div>
+                        <div className="List-info--rooms">
+                          Rooms {property.size_rooms}
+                        </div>
+                        <div>
+                          {property.link}
+                        </div>
+                        <div>
+                          {property.market_date}
+                        </div>
+                      </div>
+                    </a>
+                  ))}
+                </div>
+              )}
+            <div className='List-pagination'>
+              {pages > 0 && (
+                <ul>
+                  {Array(pages)
+                    .fill(0)
+                    .map((el, index) => (
+                      <li
+                        key={index}
+                        onClick={() => {
+                          this.handleChange('page', index + 1);
+                        }}
+                        className={`${this.params.page - 1 === index ? 'active' : ''}`}>
+                        {index + 1}
+                      </li>
+                    ))}
+                </ul>
+              )}
+            </div>
+          </div>
+
         </div>
-      </div>
+
+
+      </React.Fragment>
     );
   }
 }
